91596576e87bb2c975cff716560a4cfd80506c82
ica <irchrislee@gmail.com>
2026-01-27 17:13:21 +0900

    web comfig 수정

--- booklog/src/main/java/com/example/booklog/global/config/WebConfig.java
+++ booklog/src/main/java/com/example/booklog/global/config/WebConfig.java
@@ -10,13 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // 로컬 개발 환경 전부 커버
                 .allowedOriginPatterns(
                         "http://localhost:*",
-                        "http://127.0.0.1:*"
+                        "http://127.0.0.1:*",
+                        "https://localhost:*",
+                        "https://127.0.0.1:*"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
