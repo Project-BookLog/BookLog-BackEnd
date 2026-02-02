1248ddc8aa2caab4de63046384a211e1ad67bb0a
박준현 <millkk04@naver.com>
2026-02-02 19:03:26 +0900

    fix: transcation error

--- booklog/src/main/java/com/example/booklog/domain/search/service/IntegratedSearchService.java
+++ booklog/src/main/java/com/example/booklog/domain/search/service/IntegratedSearchService.java
@@ -9,7 +9,6 @@ import com.example.booklog.global.common.apiPayload.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -39,7 +38,6 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class IntegratedSearchService {
 
     private final AuthorSearchService authorSearchService;
