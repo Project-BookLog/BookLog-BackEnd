6daff9b621762fabf7e77b2346d2f60a764c4667
jaehyeon4406 <jaehyeon4406@gmail.com>
2026-02-11 16:27:49 +0900

    FIX: 소셜 로그인 토큰 문제 수정 / 스웨거 추가

--- booklog/src/main/java/com/example/booklog/global/common/apiPayload/handler/GlobalExceptionHandler.java
+++ booklog/src/main/java/com/example/booklog/global/common/apiPayload/handler/GlobalExceptionHandler.java
@@ -117,7 +117,8 @@ public class GlobalExceptionHandler {
             case NOT_FOUND -> HttpStatus.NOT_FOUND;
             case UNAUTHORIZED, TOKEN_EXPIRED, INVALID_TOKEN -> HttpStatus.UNAUTHORIZED;
             case FORBIDDEN -> HttpStatus.FORBIDDEN;
-            case DUPLICATE_EMAIL, INVALID_EMAIL_FORMAT, INVALID, INVALID_PASSWORD -> HttpStatus.BAD_REQUEST;
+            case DUPLICATE_EMAIL, INVALID_EMAIL_FORMAT, INVALID, INVALID_PASSWORD,
+                 KAKAO_API_ERROR, KAKAO_TOKEN_INVALID -> HttpStatus.BAD_REQUEST;
         };
     }
 
